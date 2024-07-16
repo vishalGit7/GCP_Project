d31b1e3c5e4c5223baf5902ee27cabbca4c4c762
vishak <vbadole03@gmail.com>
2024-07-16 13:53:32 +0530

    Update main.py

--- cr_helloworld/main.py
+++ cr_helloworld/main.py
@@ -1,31 +1,32 @@
-# Copyright 2020 Google, LLC.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import re
+from google.cloud import storage
+from google.cloud import bigquery
+from google.cloud.exceptions import NotFound
 
-# [START cloudbuild_python_flask]
-import os
+def hello_gcs(event, context):
+    bq_client = bigquery.Client()
+    bucket = storage.Client().bucket("your-bucket-name")
 
-from flask import Flask
+    for blob in bucket.list_blobs(prefix="your-folder-name/"):
+        if ".csv" in blob.name:
+            # Extract the file name for the BigQuery table ID
+            csv_filename = re.findall(r".*/(.*).csv", blob.name)
+            bq_table_id = "your-project.dataset-name." + csv_filename[0]
 
-app = Flask(__name__)
+            try:
+                # Check if the table already exists and skip uploading it
+                bq_client.get_table(bq_table_id)
+                print(f"Table {bq_table_id} already exists. Not uploaded.")
+            except NotFound:
+                # If the table is not found, upload it
+                uri = f"gs://your-bucket-name/{blob.name}"
+                job_config = bigquery.LoadJobConfig(
+                    autodetect=True,
+                    skip_leading_rows=1,
+                    source_format=bigquery.SourceFormat.CSV,
+                )
+                load_job = bq_client.load_table_from_uri(uri, bq_table_id, job_config=job_config)
+                load_job.result()  # Wait for the job to complete
+                print(f"Uploaded file: {blob.name} to table: {bq_table_id}")
 
-
-@app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return f"Helloo {name}!"
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-# [END cloudbuild_python_flask]
+# This function is triggered by Pub/Sub whenever there is a new file in the GCS bucket
