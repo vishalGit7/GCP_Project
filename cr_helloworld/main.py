b339b5b16f7203f17a59f9982f5621e4569556c0
vishak <vbadole03@gmail.com>
2024-07-16 16:57:07 +0530

    Update main.py

--- cr_helloworld/main.py
+++ cr_helloworld/main.py
@@ -21,11 +21,32 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
+def process_file(bucket_name, filename):
+  """Reads a CSV file from GCS and prints the content.
 
+  Args:
+    bucket_name: Name of the GCS bucket.
+    filename: Name of the CSV file in the bucket.
+  """
+  # Access environment variables (replace with your own)
+  bucket_name = os.environ.get('BUCKET_NAME')
+
+  # Download the file from GCS
+  client = storage.Client()
+  bucket = client.get_bucket(bucket_name)
+  blob = bucket.blob(filename)
+  data = blob.download_as_string()
+
+  # Print the file content
+  print(data.decode('utf-8'))  # Decode bytes to string
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-# [END cloudbuild_python_flask]
+  # Replace with environment variables or command line arguments
+  bucket_name = "winged-app-429513-b8_terraform"
+  filename = "winged-app-429513-b8_terraform/username.csv"
+
+  process_file(bucket_name, filename)
+
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+# # [END cloudbuild_python_flask]
