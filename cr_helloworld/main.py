0f217dac10970063697345fa34d7baf36ece2d98
vishak <vbadole03@gmail.com>
2024-07-16 16:05:47 +0530

    cr

--- cr_helloworld/main.py
+++ cr_helloworld/main.py
@@ -1,27 +1,31 @@
-from google.cloud import storage
+# Copyright 2020 Google, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-def process_file(bucket_name, filename):
-  """Reads a CSV file from GCS and prints the content.
+# [START cloudbuild_python_flask]
+import os
 
-  Args:
-    bucket_name: Name of the GCS bucket.
-    filename: Name of the CSV file in the bucket.
-  """
-  # Access environment variables (replace with your own)
-  bucket_name = os.environ.get('BUCKET_NAME')
+from flask import Flask
 
-  # Download the file from GCS
-  client = storage.Client()
-  bucket = client.get_bucket(bucket_name)
-  blob = bucket.blob(filename)
-  data = blob.download_as_string()
+app = Flask(__name__)
 
-  # Print the file content
-  print(data.decode('utf-8'))  # Decode bytes to string
 
-# if __name__ == "__main__":
-#   # Replace with environment variables or command line arguments
-#   bucket_name = "your-bucket-name"
-#   filename = "your-file.csv"
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return f"Hello {name}!"
 
-#   process_file(bucket_name, filename)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+# [END cloudbuild_python_flask]
