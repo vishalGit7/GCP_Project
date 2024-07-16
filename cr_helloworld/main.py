d85974365428dd2b3f4dd615b9be3915857c5d35
vishak <vbadole03@gmail.com>
2024-07-16 12:14:18 +0530

    changes

---
+++ cr_helloworld/main.py
@@ -0,0 +1,18 @@
+# [START cloudrun_helloworld_service]
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    """Example Hello World route."""
+    name = os.environ.get("NAME", "World")
+    return f"Helloo  {name}!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+# [END cloudrun_helloworld_service]
