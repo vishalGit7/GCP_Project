c9de0083ae05a791608a31977a4a463d41a20ba2
vishak <vbadole03@gmail.com>
2024-07-18 18:15:50 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,10 @@
+variable "project_id" {
+  type = string
+  description = "The Google Cloud project ID"
+}
 
 # (Optional) Define a service account for Cloud Build
 resource "google_service_account" "build_sa" {
   account_id = "cloud-build-sa"
+  project = var.project_id
 }
