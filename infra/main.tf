a4e44a007cafb09cfd0b7755403dd922affda511
vishak <vbadole03@gmail.com>
2024-07-18 18:38:32 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -9,3 +9,9 @@ resource "google_service_account" "build_sa" {
   account_id = "cloud-build-sa"
   project = var.project_id
 }
+
+resource "google_service_account_iam_member" "storage_access" {
+  role = "roles/storage.admin"  # Replace with the desired role
+  member = "serviceAccount:{{ google_service_account.build_sa.email }}"
+  project = var.project_id
+}
