142fb93c5dc6f86906eb8e3cb56dbf7a7bfd40e2
vishak <vbadole03@gmail.com>
2024-07-18 18:57:51 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -11,9 +11,9 @@ resource "google_service_account" "build_sa" {
 }
 
 resource "google_service_account_iam_member" "admin-account-iam" {
-  service_account_id = google_service_account.build_sa.name
+  service_account_id = google_service_account.build_sa.id
   role               = "roles/storage.objectCreator"
   member             = "serviceAccount:{{ google_service_account.build_sa.email }}"
-  
+
 depends_on = [ google_service_account.build_sa ]
 }
